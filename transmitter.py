bd2d0dd68fde026cef1a94ee3d8ded6cff734025
Pratool Gadtaula <pratool.gadtaula@students.olin.edu>
2015-12-13 16:50:03 -0500

    Shortened bit length. Actually works!

--- transmitter.py
+++ transmitter.py
@@ -22,9 +22,8 @@ def write_floats(filename, tx_data):
 
     return data
 
-def arr_gen(input_arr, time):
-    num_samp = int(time*SAMPLE_RATE)
-    bit_len = num_samp/len(input_arr)
+def arr_gen(input_arr):
+    bit_len = 100
     out_arr = []
     for i in range(len(input_arr)):
         out_arr.extend([input_arr[i]]*bit_len)
@@ -34,16 +33,13 @@ def arr_gen(input_arr, time):
 if __name__ == '__main__':
     time = 2
     zero_pad = 1e5
-    num_samp = int(time*SAMPLE_RATE)
     in_arr = np.array([1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0])
-    num_bits = len(in_arr)
-    tot_samp = num_samp + zero_pad
     in_arr = in_arr*2 - 1
-    x_real = arr_gen(in_arr, time)
+    x_real = arr_gen(in_arr)
     # pad with zeros
     x_real = np.hstack((np.zeros(zero_pad/2), x_real, np.zeros(zero_pad/2)))
     plt.plot(x_real)
     plt.show()
-    x_imag = np.zeros(tot_samp)
+    x_imag = np.zeros(len(x_real))
     test_data = (x_real, x_imag)
     write_floats('sent.dat', test_data)
